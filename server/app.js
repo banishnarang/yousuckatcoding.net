2192caa414d75e240234339404a9c765cd9e7950
banishnarang <bnarang2@uwo.ca>
2022-04-17 14:57:59 -0400

    Connect MongoDB

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
+import mongoose from "mongoose";
 const morgan = require("morgan");
 import { config } from "dotenv";
 config();
 
 // create express app
 const app = express();
+
+// db connection
+mongoose
+	.connect(process.env.MONGO_URI, {})
+	.then(() => console.log("-- DB connected --"))
+	.catch((err) => console.log(`DB connection error => ${err}`));
 
 // apply middlewares
 app.use(cors());
